4d24ab5238ba75c8348158432d42ef560075f9b3
Diana Strahilova <diana.strahilova91.ds@gmail.com>
2024-10-12 18:10:07 +0000

    Add summernote

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,8 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post # same as all()
     # generic view
-    queryset = Post.objects.all().order_by("-created_on") # show all authors and ordering posts
+    # queryset = Post.objects.all().order_by("-created_on") # show all authors and ordering posts
     # queryset = Post.objects.filter(author=2) # show second author / filter
+    queryset = Post.objects.filter(status=1)
+    template_name = "post_list.html"
 
